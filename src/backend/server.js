f525041235afdecee901f97399d77cf020cf9597
Aimee MacDonald <aimeelmacdonald@gmail.com>
2021-09-30 01:11:57 +0200

    React render

--- src/backend/server.js
+++ src/backend/server.js
@@ -6,5 +6,7 @@ const path = require('path');
 app.set('view engine', 'pug')
 app.set('views', __dirname)
 
+app.use(express.static(path.join(__dirname, '../../dist')))
+
 app.get('*', (req, res) => res.status(200).render('index'))
 app.listen(process.env.PORT, console.log('Server up'))
