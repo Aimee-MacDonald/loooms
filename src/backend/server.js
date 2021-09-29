a42d149cf0366d1aef4750506bdde1a8530ca6b2
Aimee MacDonald <aimeelmacdonald@gmail.com>
2021-09-30 00:27:05 +0200

    Serve a page

---
+++ src/backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const path = require('path');
+
+app.set('view engine', 'pug')
+app.set('views', __dirname)
+
+app.get('*', (req, res) => res.status(200).render('index'))
+app.listen(process.env.PORT, console.log('Server up'))
